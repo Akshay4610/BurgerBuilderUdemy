02cc1315f4f74699d48b976ce265c509d6ac61ba
Akshay Chandwani <akshaychandwani992@gmail.com>
2021-02-11 01:21:03 +0530

    feat: completed  burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render() {
         return (
             <div>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </div>
         );
